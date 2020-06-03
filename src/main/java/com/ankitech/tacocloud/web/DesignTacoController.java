691a6c3ed510c04d0aad33200c1e2d3167d4a90f
ankittech <ankitech@gmail.com>
2020-06-03 22:58:24 +0530

    added orders page

--- src/main/java/com/ankitech/tacocloud/web/DesignTacoController.java
+++ src/main/java/com/ankitech/tacocloud/web/DesignTacoController.java
@@ -6,9 +6,12 @@ import com.ankitech.tacocloud.Taco;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -37,6 +40,19 @@ public class DesignTacoController {
         }
         model.addAttribute("design", new Taco());
         return "design";
+    }
+
+    @PostMapping
+    public String processDesign(@Valid Taco design, Errors errors) {
+
+        if (errors.hasErrors()) {
+            return "design";
+        }
+        // Save the taco design...
+        // We'll do this in chapter 3
+        log.info("Processing design: " + design);
+
+        return "redirect:/orders/current";
     }
 
     private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
